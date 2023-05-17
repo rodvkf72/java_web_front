dc7c2bb5821f82c82fd80da320a299c48298f180
Kim Kwang Ho <rodvkf72@naver.com>
2023-05-17 18:02:30 +0900

    Update: Info Tab
    소개 부분 디자인 및 내용 변경

--- src/Java/user/info/myinfo.svelte
+++ src/Java/user/info/myinfo.svelte
@@ -3,10 +3,11 @@
 <script>
 import { onMount } from "svelte";
 
-
+    /*
     onMount(async() => {
         document.getElementsByClassName("detaildiv")[0].style.display="none";
     })
+    */
 </script>
 
 <header class="masthead" style="background-image: url('/resources/image/home-bg.jpg')">
@@ -24,118 +25,72 @@ import { onMount } from "svelte";
     </div>
 </header>
 
-<br>
-<br>
 <!-- Main Content -->
 <div class="cover hanna-font">
     <div class="row">
-        <div class="col-lg-4 col-md-4 mb-2"></div>
-        <div class="col-lg-2 col-md-2 mb-1 simple"><button class="simplebtn" onclick="simple()">간략한 소개</button></div>
-        <div class="col-lg-2 col-md-2 mb-1 detail"><button class="detailbtn" onclick="detail()">이력서 소개</button></div>
-        <div class="col-lg-4 col-md-4 mb-2"></div>
-        <br><br><br><br>
-        <div class="col-lg-1 col-md-6 mb-1"></div>
-        
-        <div class="col-lg-2 col-md-6 mb-2 simplelink">
-            <div class="rinks" style="font-size: 20px;">
-                <b style="font-size: 25px; color: rgba(11, 68, 175, 0.6);">Link's</b><hr width="20%;">
-                <a href="https://github.com/rodvkf72">Github.</a><br>
-                <a href="https://nice-pigeon-244.notion.site/Notion-b8486f6878a447b1b4aeaeddb3128b2c">Notion.</a>
-            </div>
-        </div>
-        <div class="col-lg-3 col-md-6 mb-3 simpledivcol1">
-            <img style="width:60%; height:50%; border-radius: 10px;" src="./Java/image/bottles.jpeg" alt="맥주병's">
-            <br><br>
-            <span style="color:grey;">맥주병수집가</span><br>
-            <b style="font-size: 40px; color:rgba(11, 68, 175, 0.6);">개발자</b><br>
-            <b>김광호</b> 입니다.
-        </div>
-        <div class="col-lg-2 col-md-6 mb-2 simpledivcol2">
+        <div class="col-lg-6 col-md-6 mb-6 center">
             <div>
-                <b style="font-size:20px; color:rgba(11, 68, 175, 0.6);">김광호 / Kim Kwang Ho</b><br>
-                <span style="font-size:15px; color:grey;">1995.01.25 / 부산광역시</span><br>
-                <br>
-                Email. <a href="mailto:rodvkf72@naver.com">rodvkf72@naver.com</a>
-                <hr>
-            </div>
-            <div>
-                <b style="font-size:20px; color:rgba(11, 68, 175, 0.6);">CAREER</b><br>
-                <br>
-                - 2021. 동의대학교 컴퓨터소프트웨어공학과 졸업 (학사)
-                <br>
-                - 2022. SI 웹 백엔드 개발 (1년)
-                <hr>
-            </div>
-            <div>
-                <b style="font-size:20px; color:rgba(11, 68, 175, 0.6);">ABOUT</b><br>
-                <br>
-                학부 시절 중 메일 시스템 유지/보수 프로젝트에서 백엔드 비즈니스 로직 개발/수정, 데이터 가공에 흥미를 가지게 되어 백엔드 개발자를 목표로 하고 있습니다.
-            </div>
-        </div>
-        <div class="col-lg-2 col-md-6 mb-2 simpledivcol3">
-            <div>
-                <b style="font-size:20px; color:rgba(11, 68, 175, 0.6);">LANGUAGE</b>
-                <br><br>
-                - Java
-                <br>
-                - Go
-                <hr>
-            </div>
-            <div>
-                <b style="font-size:20px; color:rgba(11, 68, 175, 0.6);">FRAMEWORK</b>
-                <br><br>
-                - Spring (Java)
-                <br>
-                - Echo (Go)
-                <hr>
-            </div>
-            <div>
-                <b style="font-size:20px; color:rgba(11, 68, 175, 0.6);">DATABASE</b>
-                <br><br>
-                - MySQL
-                <br>
-                - Oracle
-                <hr>
-            </div>
-            <div>
-                <b style="font-size:20px; color:rgba(11, 68, 175, 0.6);">ENVIRONMENT</b>
-                <br><br>
-                - Oracle Cloud
-                <br>
-                - Docker
-            </div>
-        </div>
-        <div class="col-lg-7 col-md-6 mb-7 detaildiv">
-            <div class="col-lg-1 col-md-6 mb-1" style="display:inline-block;"></div>
-            <div class="col-lg-3 col-md-6 mb-3 detaildivcol1">
-                <div>
-                    <span style="font-size:30px;">김 광 호</span>
+                <p style="line-height: 0.8; margin:30px 0; font-size: 24px; !important">
+                    <b>Kim Kwang Ho </b><span style="font-size: 17px;">(95.01.25)</span>
                     <br>
-                    <span style="color:orange;">백엔드 개발자</span>
-                </div>
-            </div>
-            <div class="col-lg-7 col-md-6 mb-7 detaildivcol2">
-                <hr style="border-color:black; border-top: 3px solid; margin-left:0; width:100%; padding-right:16%; display:inline-block;">
-                <div>
-                    <span style="font-size:20px;"><b>김광호</b></span><br>
-                    부산광역시<br>
+                    <span style="font-size: 14px; color: grey;">Kyobo Information & Communication Co.Ltd</span>
                     <br>
-                    <span style="color:orange">rodvkf72@naver.com</span><br>
-                </div>
+                    <span style="font-size: 14px; color: grey;">Insurance Business Dept (STAFF)</span>
+                </p>
+                <p style="margin:30px 0px 0px 0px; !important">
+                    WEB Back-End Programmer
+                </p>
+                <hr width="20%">
+                <p style="margin:70px 0px 0px 0px; !important">
+                    Languages & Databases
+                </p>
+                <hr width="20%">
+                <p>
+                    <img src="https://img.shields.io/badge/java-%23ED8B00.svg?style=for-the-badge&logo=java&logoColor=white" alt="Java" style="width: 60px; height: 30px;">
+                    <img src="https://img.shields.io/badge/spring-%236DB33F.svg?style=for-the-badge&logo=spring&logoColor=white" alt="Spring" style="width: 60px; height: 30px;">
+                    <img src="https://img.shields.io/badge/go-%2300ADD8.svg?style=for-the-badge&logo=go&logoColor=white" alt="Go" style="width: 60px; height: 30px;">
+                    <img src="https://img.shields.io/badge/javascript-%23323330.svg?style=for-the-badge&logo=javascript&logoColor=%23F7DF1E" alt="JavaScript" style="width: 60px; height: 30px;">
+                </p>
+                <p>
+                    <img src="https://img.shields.io/badge/Oracle-F80000?style=for-the-badge&logo=oracle&logoColor=white" alt="Oracle" style="width: 60px; height: 30px;">
+                    <img src="https://img.shields.io/badge/mysql-%2300f.svg?style=for-the-badge&logo=mysql&logoColor=white" alt="MySQL" style="width: 60px; height: 30px;">
+                    <img src="https://img.shields.io/badge/MariaDB-003545?style=for-the-badge&logo=mariadb&logoColor=white" alt="MariaDB" style="width: 60px; height: 30px;">
+                    <img src="https://img.shields.io/badge/MSSQL-F80000.svg?&style=for-the-badge&logo=microsoftsqlserver&logoColor=white" alt="MSSQL" style="width: 60px; height: 30px;">
+                </p>
+                <p style="margin:70px 0px 0px 0px; !important">
+                    Tools
+                </p>
+                <hr width="20%">
+                <p>
+                    <img src="https://img.shields.io/badge/Eclipse-2C2255.svg?&style=for-the-badge&logo=eclipseide&logoColor=white" alt="Eclipse" style="width: 60px; height: 30px;">
+                    <img src="https://img.shields.io/badge/VSCode-5C2D91.svg?&style=for-the-badge&logo=visualstudiocode&logoColor=white" alt="VisualStudioCode" style="width: 60px; height: 30px;">
+                    <img src="https://img.shields.io/badge/Android-3DDC84.svg?&style=for-the-badge&logo=android&logoColor=white" alt="Android" style="width: 60px; height: 30px;">
+                    <img src="https://img.shields.io/badge/Git-F05032.svg?&style=for-the-badge&logo=git&logoColor=white" alt="Git" style="width: 60px; height: 30px;">
+                </p>
+                <p>
+                    <img src="https://img.shields.io/badge/jira-0052CC.svg?&style=for-the-badge&logo=jira&logoColor=white" alt="Jira" style="width: 60px; height: 30px;">
+                    <img src="https://img.shields.io/badge/slack-4A154B.svg?&style=for-the-badge&logo=slack&logoColor=white" alt="Slack" style="width: 60px; height: 30px;">
+                    <img src="https://img.shields.io/badge/Notion-000000.svg?&style=for-the-badge&logo=notion&logoColor=white" alt="Notion" style="width: 60px; height: 30px;">
+                    <img src="https://img.shields.io/badge/FileZilla-BF0000.svg?&style=for-the-badge&logo=filezilla&logoColor=white" alt="FileZilla" style="width: 60px; height: 30px;">
+                </p>
             </div>
-            <div class="col-lg-1 col-md-6 mb-1" style="display:inline-block;"></div>
+        </div>
+        <div class="col-lg-6 col-md-6 mb-6 center">
             <br>
-            <hr style="border-color:black; border-top: 3px solid; margin-left:11%; width:4%; display:inline-block;">
-            <hr style="border-color:black; border-top: 3px solid; margin-left:21%; width:63%; display:inline-block;">
-            <div class="col-lg-1 col-md-6 mb-1" style="display:inline-block;"></div>
-            <div class="col-lg-3 col-md-6 mb-3 detaildivcol1">
-                <div>
-                    기술
-                </div>
+            <div style="width: 100%; height: 30%; display:table-cell; vertical-align:middle;">
+                <img style="height: 100%;" src="https://github-readme-stats.vercel.app/api/pin/?username=rodvkf72&repo=rodvkf72" alt="github_repo_JavaWeb"/>
+            </div>
+            <br><br>
+            <div style="width: 100%; height: 30%; display:table-cell; vertical-align:middle;">
+                <img style="height: 100%;" src="https://github-readme-stats.vercel.app/api?username=rodvkf72&show_icons=true&theme=dark" alt="github_starts"/>
+            </div>
+            <br><br>
+            <div style="width: 100%; height: 30%; display:table-cell; vertical-align:middle;">
+                <img style="width: 100%;" src="http://mazassumnida.wtf/api/v2/generate_badge?boj=rodvkf72" alt="baekjoon_tier"/>
             </div>
         </div>
-        <div class="col-lg-2 col-md-6 mb-2"></div>
     </div>
 </div>
 <br>
 <br>
+<hr>
